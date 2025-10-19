406a2c1f0b9fa9ef8538899c957ff8288edf06c1
yunnzi040 <zeongyunzi@gmail.com>
2025-10-19 16:39:09 +0900

    feat(calculator): 기본 입력 처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 사용자로부터 문자열 입력 받기 (입력 문자열 앞뒤 공백 제거)
+        String input = Console.readLine().trim();
+
+        // null 또는 빈 문자열("") 입력 시 0 반환
+        if (input.isBlank()) {
+            System.out.println("0");
+        } else {
+            System.out.println(input);
+        }
     }
 }
