fd58729e4cd9d311966d4a94b20200441c32bac2
yunnzi040 <zeongyunzi@gmail.com>
2025-10-19 16:40:08 +0900

    test(calculator): 기본 입력 처리 테스트 케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,13 +1,29 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+    @Test
+    void 공백_입력_테스트() {
+        assertSimpleTest(() -> {
+            run(" ");  // 빈 문자열일 경우 'No line found' 발생
+            assertThat(output()).contains("0");
+        });
+    }
+
+    @Test
+    void 공백_제거_테스트() {
+        assertSimpleTest(() -> {
+            run(" 1,2,3 ");  // 공백 포함 입력
+            assertThat(output()).contains("1,2,3");
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -19,8 +35,8 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
